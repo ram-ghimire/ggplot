95ac48f93b3e345a3599f798e77bb9148e5689bb
ram-ghimire <ramghimire71@gmail.com>
2020-04-01 18:34:32 +1100

    Practice at home

--- gapminder_ggplot.R
+++ gapminder_ggplot.R
@@ -1,6 +1,6 @@
 install.packages("tidyverse")
 library(tidyverse)
-gapminder <- read_csv("data/gapminder_data.csv")
+gapminder <- read_csv("gapminder.csv")
 gapminder_1977 <- filter(gapminder, year == 1977)
 ggplot(data=gapminder_1977)+
   geom_point(mapping=aes(x=gdpPercap,y=lifeExp, colour=continent, size=pop))
